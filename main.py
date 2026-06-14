adc9c5f6e103509571078eb97962efc4f370274d
Satya Saurav Satapathy <satyasauravsatapathy@gmail.com>
2026-06-14 17:04:51 +0530

    Addition of Codes

---
+++ main.py
@@ -0,0 +1,39 @@
+# API Authentication
+
+import requests
+from twilio.rest import Client
+import os
+
+api_key = os.getenv("OPENWEATHER_API_KEY")
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+
+weather_parameters = {
+    "lat": 60.128162,
+    "lon": 18.643501,
+    "cnt": 4,
+    "appid": api_key
+}
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=weather_parameters)
+print(response)
+response.raise_for_status()
+
+data = response.json()
+print(data["list"][0]["weather"][0]["id"])
+
+will_rain = False
+for hour_data in data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain, get an umbrella with you!",
+        from_="FROM_NUMBER",
+        to="TO_NUMBER",
+    )
+    print(message.sid)
